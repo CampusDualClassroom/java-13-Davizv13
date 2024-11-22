85e37daf5186907771fac61d67df20a87191cf7d
David Quintas Viana <david.quintas.v13@gmail.com>
2024-11-22 14:54:24 +0100

    Ejercicio 13 resuelto

--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -11,4 +11,16 @@ public class FuelTank {
   public int getActualFuel() {
     return this.actualFuel;
   }
+
+  public void setActualFuel(int fuel) {
+    if (fuel >= 0) {
+      this.actualFuel = fuel;
+      System.out.println("La capacidad de combustible ha sido actualizada a " + this.actualFuel + " litros.");
+    } else {
+      System.out.println("No se puede establecer un valor negativo para el combustible.");
+    }
+  }
+
+
+
 }
